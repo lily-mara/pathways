82451d6c70bafdd272d27ac3e70a65514fd40c42
Lily Mara <lilymara@fastmail.com>
2022-01-24 21:06:13 -0800

    make it more interactive

--- sketch.js
+++ sketch.js
@@ -1,49 +1,72 @@
+let listOfLineCollections = [];
+let activeLineCollection = [];
+let x;
+let y;
+const xSteps = 10;
+let xSectionWidth;
+
 function setup() {
+  x = 0;
+  y = windowHeight / 2;
+
+  xSectionWidth = windowWidth / xSteps;
+
   createCanvas(windowWidth, windowHeight);
+
+  setInterval(() => {
+    listOfLineCollections.push(activeLineCollection);
+    activeLineCollection = genLines(x, 0, x + xSectionWidth, windowHeight, y);
+  }, 500);
 
   background(256);
 
-  stroke(200);
-  for (let i = 0; i < 20; i++) {
-    web(0, 0, windowWidth / 2, windowHeight);
-  }
-
-  stroke(0);
-  webWithPredefinedEnd(0, 0, windowWidth / 2, windowHeight);
-
-  stroke(200);
-  for (let i = 0; i < 20; i++) {
-    web(windowWidth / 2, 0, windowWidth, windowHeight);
-  }
-
-  stroke(0);
-  web(windowWidth / 2, 0, windowWidth, windowHeight);
+  activeLineCollection = genLines(x, 0, x + xSectionWidth, windowHeight, y);
 }
 
-function web(x1, y1, x2, y2, stepCount) {
-  const xStepCount = stepCount || 100;
+function keyPressed() {
+  let [x1, y1, x2, y2] = activeLineCollection[activeLineCollection.length - 1];
+
+  x += xSectionWidth;
+  y = y2;
+}
+
+function draw() {
+  background(256);
+
+  stroke(200);
+  for (let lineCollection of listOfLineCollections) {
+    drawLines(lineCollection);
+  }
+
+  stroke(0);
+  drawLines(activeLineCollection);
+}
+
+function drawLines(lines) {
+  for (let [x1, y1, x2, y2] of lines) {
+    line(x1, y1, x2, y2);
+  }
+}
+
+function genLines(x1, y1, x2, y2, y) {
+  const xStepCount = 100;
 
   const xStep = (x2 - x1) / xStepCount;
   const yStep = (y2 - y1) / 100;
 
-  let y = (y1 + y2) / 2;
   let x = x1;
+
+  const lines = [];
 
   for (let i = 0; i < xStepCount; i++) {
     const diff = (random(2) - 1) * yStep;
     const newY = y + diff;
 
-    line(x, y, x + xStep, newY);
+    lines.push([x, y, x + xStep, newY]);
 
     y = newY;
     x += xStep;
   }
 
-  return [x, y];
-}
-
-function webWithPredefinedEnd(x1, y1, x2, y2) {
-  const [x, y] = web(x1, y1, x2, y2, 99);
-
-  line(x, y, x2, (y1 + y2) / 2);
+  return lines;
 }
